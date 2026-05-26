f889b469840f52e6aeb651e49d0336ad695acf3d
Bruce <love890313@gmail.com>
2026-05-26 16:06:51 +0800

    03

--- 03/Program.cs
+++ 03/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("輸入一個有意義的英文敘述字串");
+            var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var output = input.GroupBy(x => x, StringComparer.OrdinalIgnoreCase);//大小寫忽略
+            if (input.Length>0)
+            {
+                foreach (var group in output)
+                {
+                    Console.WriteLine($"字串: {group.Key}，出現了 {group.Count()} 次");
+                }
+            }
+            else
+            {
+                Console.Write("沒東西謝謝:)");
+            }
         }
     }
 }
